8c7716aa2dddfe5bf25b29fe332144fcaab2c0bd
nigeltiany <nigeltiany@gmail.com>
2022-09-03 16:41:27 -0400

    prepare for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'NCAT IEEE HW COMP',
   tagline: 'Aggies IEEE Hardware Competition',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://ncat-hw-comp.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ncat-hw-comp', // Usually your GitHub org/user name.
-  projectName: 'Docs', // Usually your repo name.
+  projectName: 'ncat-hw-comp.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
